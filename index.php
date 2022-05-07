eb657ee2743dd660c8b3d743d0b437d6f3435314
Martin <martbony@outlook.fr>
2022-05-07 22:15:41 +0200

    ui

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
         <main id="scanButton"><div class="circles"><span></span></div><h1>Taper pour scanner</h1></main>
         <main id="scanningPage"><div class="circles"><span></span><span></span><span></span></div><h1>Scannez maintenant</h1></main>
         <main id="waitingPage"><h1>En attente des résultats</h1></main>
-        <main id="resultsPage"><h1><span id="nombre">0</span><span id="super">ème</span>verre</h1><button>Scanner de nouveau</button></main>
+        <main id="resultsPage"><h1><span id="nombre">0</span><span id="super">ème</span> verre</h1><button>Scanner de nouveau</button></main>
         <main id="errorPage"><h1>Operation Impossible (Ne fonctionne pas sur safari ou firefox)</h1></main>
         <script src="script.js"></script>
     </section>
