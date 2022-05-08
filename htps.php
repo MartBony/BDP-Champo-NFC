e373302337e2c752a12975ef33f2bd28c6ef0be6
Martin <martbony@outlook.fr>
2022-05-08 16:01:45 +0200

    r

---
+++ htps.php
@@ -0,0 +1,9 @@
+<?php
+echo $_SERVER['HTTPS'];
+echo $_SERVER['SERVER_NAME']
+
+if ((!isset($_SERVER['HTTPS']) || $_SERVER['HTTPS'] != "on") && $_SERVER['SERVER_NAME'] != "localhost") {
+    echo $_SERVER['HTTPS'];
+}
+
+?>
