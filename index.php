a4381d38a317233a2267d134ccb78cb86d422100
Martin <martbony@outlook.fr>
2022-05-08 14:27:21 +0200

    https

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
+<?php
+	// Require https
+	if ((!isset($_SERVER['HTTPS']) || $_SERVER['HTTPS'] != "on") && $_SERVER['SERVER_NAME'] != "localhost") {
+		$url = "https://". $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
+		header("Location: $url", true, 301);
+		exit;
+	}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
